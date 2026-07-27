aeee785692c17341e0061ec18c84540be0f5b4ce
connectwithsahiljain <connectwithsahiljain@gmail.com>
2026-07-27 14:48:04 +0530

    jenkins file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello from devops ci/cd pipeline"
+    return "Hello from devops ci/cd pipeline, docker img v1"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
